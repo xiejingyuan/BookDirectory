1139ee14082591a0751cc0622e40c3f9b5b89bbd
xiejingyuan <tiger_yoyo@outlook.com>
2025-07-13 14:52:05 -0400

    fix bug

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Book = require('../models/Book');
+const Book = require('../models/book');
 
 // Middleware: check if user is logged in
 function isLoggedIn(req, res, next) {
